c8dfb15db6b9cbe2949954c9ca71cefe68406ffb
Thiago Izidoro <thiagomachadox@gmail.com>
2022-01-28 15:40:17 -0300

    fix: fix category repository

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { request, Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../src/modules/cars/usecases/CreateCategory";
+import createCategoryController from "../src/modules/cars/usecases/CreateCategory";
 import { listCategoriesController } from "../src/modules/cars/usecases/ListCategories";
 import { importCategoryController } from "../src/modules/cars/usecases/ImportCategory";
 
@@ -12,7 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
-    return createCategoryController.handle(request, response);
+    return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
