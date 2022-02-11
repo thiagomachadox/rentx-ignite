a05d7f73e363d914c93994c94f311772e51de865
Thiago Izidoro <thiagomachadox@gmail.com>
2022-02-11 15:38:45 -0300

    feat: encrypt password when persisting user to database and check if email already exists

--- src/modules/accounts/usecases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/usecases/CreateUser/CreateUserUseCase.ts
@@ -1,4 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -15,11 +18,19 @@ class CreateUserUseCase {
         driver_license,
         password,
     }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
             email,
             driver_license,
-            password,
+            password: passwordHash,
         });
     }
 }
