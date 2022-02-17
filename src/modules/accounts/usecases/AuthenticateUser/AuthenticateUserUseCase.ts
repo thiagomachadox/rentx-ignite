a94438b41ae1c94b08dceeaf1b2a44a1317422ab
Thiago Izidoro <thiagomachadox@gmail.com>
2022-02-17 19:03:38 -0300

    feat: add error handling strategy

--- src/modules/accounts/usecases/AuthenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/usecases/AuthenticateUser/AuthenticateUserUseCase.ts
@@ -3,6 +3,7 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
+import { AppError } from "../../../../errors/AppError";
 
 interface IRequest {
     email: string;
@@ -29,13 +30,13 @@ class AuthenticateUserUseCase {
         const user = await this.usersRepository.findByEmail(email);
 
         if (!user) {
-            throw new Error("Email or password incorrect");
+            throw new AppError("Email or password incorrect");
         }
 
         // senha esta correta?
         const passwordMatch = await compare(password, user.password);
         if (!passwordMatch) {
-            throw new Error("Email or password incorrect");
+            throw new AppError("Email or password incorrect");
         }
 
         // gerar JsonWebToken (JWT)
--- src/modules/cars/usecases/CreateCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/usecases/CreateCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -18,7 +19,7 @@ class CreateCategoryUseCase {
             await this.categoriesRepository.findByName(name);
 
         if (categoryAlreadyExists) {
-            throw new Error("Category already exists");
+            throw new AppError("Category already exists");
         }
 
         this.categoriesRepository.create({ name, description });
